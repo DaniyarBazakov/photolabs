dd5471314313e626d8439b8ee492634bda6f00c2
Daniyar Bazakov <bazakovd@gmail.com>
2024-09-13 18:58:10 -0400

    Improve alt text for img and semantic structure in PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,14 +4,14 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
-      <img src={props.photo.imageSource} alt="Sample" className="photo-list__image" />
+      <img src={props.photo.imageSource} alt="User's photo" className="photo-list__image" />
       <div className="photo-list__user-details">
-        <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
+        <img src={props.photo.profile} alt="User's profile picture" className="photo-list__user-profile" />
         <div className="photo-list__user-info">
-          <h4>{props.photo.username}</h4>
-          <p className="photo-list__user-location">
+          <span>{props.photo.username}</span>
+          <span className="photo-list__user-location">
             {`${props.photo.location.city}, ${props.photo.location.country}`}
-          </p>
+          </span>
         </div>
       </div>
     </div>
