fe88f88189e67813584764235390404dabba80fb
Daniyar Bazakov <bazakovd@gmail.com>
2024-09-13 18:52:46 -0400

    Add CSS class names to PhotoListItem component to apply styles from SCSS file

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = (props) => {
-  /* Insert React */
   return (
-    <div className="photo-list-item">
-      <img src={props.photo.imageSource} alt="Sample"/>
-      <img src={props.photo.profile} alt="Profile"/>
-      <h4>{props.photo.username}</h4>
-      <p>{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
+    <div className="photo-list__item">
+      <img src={props.photo.imageSource} alt="Sample" className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <h4>{props.photo.username}</h4>
+          <p className="photo-list__user-location">
+            {`${props.photo.location.city}, ${props.photo.location.country}`}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
