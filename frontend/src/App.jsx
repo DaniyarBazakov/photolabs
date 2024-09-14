11fe2e981988fb3c10bdab518602779be0e5387c
Daniyar Bazakov <bazakovd@gmail.com>
2024-09-13 21:01:11 -0400

    Render TopicList in App to display multiple TopicListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
  // import PhotoListItem from './components/PhotoListItem';
  import PhotoList from './components/PhotoList';
+ import TopicList from './components/TopicList';
  import './App.scss';
 
  const App = () => (
    <div className="App">
      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+     <TopicList />
      <PhotoList/>
    </div>
  )
