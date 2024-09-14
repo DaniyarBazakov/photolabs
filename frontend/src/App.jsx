ef02cc970c550dc69cd2c404d4c5028917e8afc8
Daniyar Bazakov <bazakovd@gmail.com>
2024-09-14 16:04:18 -0400

    Refactore HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import PhotoList from "./components/PhotoList";
 import TopicList from "./components/TopicList";
 import TopNavigationBar from "./components/TopNavigationBar";
-import HomeRoute from "./components/HomeRoute";
+import HomeRoute from "./routes/HomeRoute";
 import "./App.scss";
 
 const App = () => (
